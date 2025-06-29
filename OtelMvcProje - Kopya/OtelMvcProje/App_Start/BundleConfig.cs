b2218c3348b9bc6b03a2fc86aa937c36fe8bab5e
bengisuazbnt <bengisuazbnt@gmail.com>
2025-06-29 03:07:48 +0300

    Otel Rezervasyon Sistemi Mvc5
    Müşteri paneli C# projesine bağlıdır ve rezervasyonlar. mesajlar oraya düşmektedir.

---
+++ OtelMvcProje - Kopya/OtelMvcProje/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OtelMvcProje
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.7.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-2.8.3.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.bundle.min.js")); // ← DİKKAT! .bundle olan
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+            BundleTable.EnableOptimizations = false;
+
+        }
+    }
+}
